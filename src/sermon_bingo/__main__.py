54a7fbfea961f153fa2f3160d9a6efbd4a0c30fe
Caleb Courtney <caleb-courtney@pluralsight.com>
2024-11-10 22:24:21 -0800

    add initial code

---
+++ src/sermon_bingo/__main__.py
@@ -0,0 +1,145 @@
+from argparse import ArgumentParser
+from collections import Counter
+from collections import defaultdict
+import random
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+# plotting the data
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+CUSTOM_WORDS = set([
+    "",
+    "v",
+    "was",
+    "us",
+    "this",
+    "said",
+])
+
+STOPWORDS = set(stopwords.words('english')) | CUSTOM_WORDS
+
+
+def _download_sermon(url: str) -> str:
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"}
+    response = requests.request("GET", url, headers=headers)
+    return response.text
+
+
+def _dedupe_words_with_same_stems(words: list[str]) -> dict[str, int]:
+    stemmer = PorterStemmer()
+    stem_dict = defaultdict(list)
+
+    words = [word for word in words if word.lower() not in STOPWORDS]
+
+    # Group words by their stems
+    for word in words:
+        stem = stemmer.stem(word)
+        stem_dict[stem].append(word)
+
+    output = Counter()
+    for _, similar_words in stem_dict.items():
+        c = Counter(similar_words)
+        output[c.most_common(1)[0][0]] = len(similar_words)
+
+    return output
+
+
+def _arrange_into_sublists(lst, n):
+    """Arranges a list into sublists of size n."""
+    return [lst[i:i + n] for i in range(0, len(lst), n)]
+
+
+def _replace_common_words_with_blanks(words, limit=3):
+    if not limit:
+        return words
+
+    counter = Counter()
+    word_frequencies = {}
+    with open("data/count_1w.txt", "r") as in_file:
+        for line in in_file:
+            word, count = line.split()
+            word_frequencies[word] = int(count)
+
+    counter = Counter({
+        word: word_frequencies.get(word, 0)
+        for word in words
+    })
+
+    n_common = [word for word, _ in counter.most_common(limit)]
+
+    output = [
+        word if word not in n_common else ""
+        for word in words
+    ]
+
+    return output
+
+
+def _parse_words(text: str, limit: int) -> list[list[str]]:
+    soup = BeautifulSoup(text, 'html.parser')
+
+    words = "\n".join(
+        x.get_text()
+        for x in soup.find_all("p")
+    ).split()
+    words = ["".join(char for char in word if char.isalpha()) for word in words]
+
+    counter = _dedupe_words_with_same_stems(words)
+
+    common_sermon_words = [word for word, _ in counter.most_common(25)]
+    random.shuffle(common_sermon_words)
+    common_sermon_words = _replace_common_words_with_blanks(common_sermon_words, limit)
+    grouped_words = _arrange_into_sublists(common_sermon_words, 5)
+
+    return grouped_words
+
+
+def _save_to_pdf(data):
+    fig, ax = plt.subplots(figsize=(5,5))
+    ax.axis('off')
+    # Create the table
+    the_table = ax.table(cellText=data, loc='center', cellLoc='center')
+
+    # Set row and column sizes to fill the figure area
+    n_rows = len(data)
+    n_cols = len(data[0])
+
+    # Increase cell height and width to fill the page more
+    for i in range(n_rows):
+        for j in range(n_cols):
+            cell = the_table[i, j]
+            cell.set_height(1.0 / n_rows)  # Adjust height proportionally
+            cell.set_width(1.0 / n_cols)   # Adjust width proportionally
+
+    # Save the table to a PDF file
+    pp = PdfPages("data/example_output.pdf")
+    pp.savefig(fig, bbox_inches='tight')  # Use bbox_inches to remove extra white space
+    pp.close()
+
+
+def _parse_args():
+    argument_parser = ArgumentParser()
+    argument_parser.add_argument("url")
+    argument_parser.add_argument("--empty-boxes", type=int, default=3)
+    args = argument_parser.parse_args()
+
+    return args
+
+
+def main():
+    args = _parse_args()
+    text = _download_sermon(args.url)
+    grouped_words = _parse_words(text, args.empty_boxes)
+    _save_to_pdf(grouped_words)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_main.py
@@ -0,0 +1,132 @@
+from unittest.mock import MagicMock
+from unittest.mock import patch
+import pytest
+
+from src.sermon_bingo.__main__ import _dedupe_words_with_same_stems
+from src.sermon_bingo.__main__ import _arrange_into_sublists
+from src.sermon_bingo.__main__ import _replace_common_words_with_blanks
+from src.sermon_bingo.__main__ import _parse_words
+from src.sermon_bingo.__main__ import main
+
+
+@pytest.fixture
+def response():
+    return """
+    <p>To be, or not to be, that is the question:</p>
+    <p>Whether 'tis nobler in the mind to suffer</p>
+    <p>The slings and arrows of outrageous fortune,</p>
+    <p>Or to take arms against a sea of troubles</p>
+    <p>And by opposing end them. To die—to sleep,</p>
+    <p>No more; and by a sleep to say we end</p>
+    <p>The heart-ache and the thousand natural shocks</p>
+    <p>That flesh is heir to: 'tis a consummation</p>
+    <p>Devoutly to be wish'd. To die, to sleep;</p>
+    <p>To sleep, perchance to dream—ay, there's the rub:</p>
+    <p>For in that sleep of death what dreams may come,</p>
+    <p>When we have shuffled off this mortal coil,</p>
+    <p>Must give us pause—there's the respect</p>
+    <p>That makes calamity of so long life.</p>
+    <p>For who would bear the whips and scorns of time,</p>
+    <p>Th'oppressor's wrong, the proud man's contumely,</p>
+    <p>The pangs of dispriz'd love, the law's delay,</p>
+    <p>The insolence of office, and the spurns</p>
+    <p>That patient merit of th'unworthy takes,</p>
+    <p>When he himself might his quietus make</p>
+    <p>With a bare bodkin? Who would fardels bear,</p>
+    <p>To grunt and sweat under a weary life,</p>
+    <p>But that the dread of something after death,</p>
+    <p>The undiscovere'd country, from whose bourn</p>
+    <p>No traveller returns, puzzles the will,</p>
+    <p>And makes us rather bear those ills we have</p>
+    <p>Than fly to others that we know not of?</p>
+    <p>Thus conscience doth make cowards of us all,</p>
+    <p>And thus the native hue of resolution</p>
+    <p>Is sicklied o'er with the pale cast of thought,</p>
+    <p>And enterprises of great pith and moment</p>
+    <p>With this regard their currents turn awry</p>
+    <p>And lose the name of action.</p>
+    """
+
+
+@pytest.mark.parametrize(
+    "words, expected",
+    [
+        (["hello", "world", "hello", "world"], {"hello": 2, "world": 2}),
+        (["hello", "world", "world", "world"], {"hello": 1, "world": 3}),
+    ],
+)
+def test_dedupe_words_with_same_stems(words, expected):
+    assert _dedupe_words_with_same_stems(words) == expected
+
+
+@pytest.mark.parametrize(
+    "words, count, expected",
+    [
+        (["hello", "world", "hello", "world"], 2, [["hello", "world"], ["hello", "world"]]),
+        (["hello", "world", "hello", "world"], 3, [["hello", "world", "hello"], ["world"]]),
+    ]
+)
+def test_arrange_into_sublists(words, count, expected):
+    assert _arrange_into_sublists(words, count) == expected
+
+@pytest.mark.parametrize(
+    "words, limit, expected",
+    [
+        (["a", "the", "hello", "world"], None, ["a", "the", "hello", "world"]), # default behavior
+        (["a", "the", "hello", "world"], 1, ["a", "", "hello", "world"]),
+        (["a", "the", "hello", "world"], 2, ["", "", "hello", "world"]),
+        (["a", "the", "hello", "world"], 3, ["", "", "hello", ""]),
+        (["a", "the", "hello", "world"], 4, ["", "", "", ""]),
+    ]
+)
+def test_replace_common_words_with_blanks(words, limit, expected):
+    assert _replace_common_words_with_blanks(words, limit) == expected
+
+
+def test_parse_words(response):
+    grouped_words = _parse_words(response, 3)
+    flattened = set()
+    for words in grouped_words:
+        flattened |= set(words)
+
+    assert flattened == {
+        'fortune',
+        'say',
+        'sleep',
+        '',
+        'arrows',
+        'opposing',
+        'suffer',
+        'end',
+        'nobler',
+        'bear',
+        'slings',
+        'arms',
+        'sea',
+        'troubles',
+        'outrageous',
+        'Thus',
+        'death',
+        'dieto',
+        'mind',
+        'tis',
+        'makes',
+        'question',
+        'Whether',
+    }
+
+
+def test_main(response):
+    mock_args = MagicMock()
+    mock_args.url = "https://google.com"
+    mock_args.empty_boxes = 3
+    with (
+        patch("src.sermon_bingo.__main__._parse_args", return_value=mock_args) as mock_parse_args,
+        patch("src.sermon_bingo.__main__._download_sermon", return_value=response) as mock_download_sermon,
+        patch("src.sermon_bingo.__main__._save_to_pdf") as mock_save_to_pdf,
+    ):
+        main()
+
+    mock_parse_args.assert_called_once()
+    mock_download_sermon.assert_called_once_with(mock_args.url)
+    mock_save_to_pdf.assert_called_once()
